a2a95759e158fbc3c5ce67bc7a32c4a083cf1976
chris <christophe.allan.simon@gmail.com>
2024-08-01 21:14:12 +0200

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,57 @@
+<script setup>
+import logo from '@/assets/img/logo.png';
+
+
+</script>
+<template>
+  <body class="bg-gray-100">
+    <nav class="bg-white shadow-md">
+        <div class="container mx-auto px-4">
+            <ul class="flex justify-around items-center py-4">
+                <li class="text-gray-500 hover:text-gray-700">
+                    <a href="#">
+                        <img src="../assets/img/home.png" alt="Home" class="inline-block w-6 h-6">
+                        <span class="ml-2">Home</span>
+                    </a>
+                </li>
+                <li class="text-gray-500 hover:text-gray-700">
+                    <a href="#" class="flex items-center">
+                        <img src="../assets/img/pokedex.png" alt="Pokedex" class="inline-block w-6 h-6">
+                        <span class="ml-2">Pokedex</span>
+                    </a>
+                </li>
+                <li class="text-gray-500 hover:text-gray-700">
+                    <a href="#" class="flex items-center">
+                        <img src="https://example.com/videogames-icon.png" alt="Videogames" class="inline-block w-6 h-6">
+                        <span class="ml-2">Videogames</span>
+                    </a>
+                </li>
+                <li class="text-gray-500 hover:text-gray-700">
+                    <a href="#" class="flex items-center">
+                        <img src="https://example.com/gcc-pokemon-icon.png" alt="GCC Pokemon" class="inline-block w-6 h-6">
+                        <span class="ml-2">GCC Pokemon</span>
+                    </a>
+                </li>
+                <li class="text-gray-500 hover:text-gray-700">
+                    <a href="#" class="flex items-center">
+                        <img src="https://example.com/tv-pokemon-icon.png" alt="TV Pokemon" class="inline-block w-6 h-6">
+                        <span class="ml-2">TV Pokemon</span>
+                    </a>
+                </li>
+                <li class="text-gray-500 hover:text-gray-700">
+                    <a href="#" class="flex items-center">
+                        <img src="https://example.com/play-pokemon-icon.png" alt="Play Pokemon" class="inline-block w-6 h-6">
+                        <span class="ml-2">Play! Pokemon</span>
+                    </a>
+                </li>
+                <li class="text-gray-500 hover:text-gray-700">
+                    <a href="#" class="flex items-center">
+                        <img src="https://example.com/news-icon.png" alt="News" class="inline-block w-6 h-6">
+                        <span class="ml-2">News</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  </body>
+</template>
